d0c2be6ff4513a60b12d3c4995d4c58f6c45cda6
med-aziz-bejaoui <147663459+med-aziz-bejaoui@users.noreply.github.com>
2023-10-30 19:32:30 +0000

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@book-stor-mern.fspfw3h.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://root:<password>@book-stor-mern.fspfw3h.mongodb.net/?retryWrites=true&w=majority';
 
